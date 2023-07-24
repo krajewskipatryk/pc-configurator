b1b6e54fe8df0d8258863967f93cbbb99b17785f
krajewskipatryk <krajewskipatryk@onet.pl>
2023-07-24 04:29:14 +0200

    Initial commit

---
+++ src/main/java/com/wisepc/wisepc/WisePcApplication.java
@@ -0,0 +1,13 @@
+package com.wisepc.wisepc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisePcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisePcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisepc/wisepc/WisePcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisepc.wisepc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisePcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
